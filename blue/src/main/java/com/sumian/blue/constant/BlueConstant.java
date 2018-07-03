cb59b081e2df3c3f8024f1336871df3743df1ff4
uni7corn <yqicode@gmail.com>
2018-07-03 13:28:34 +0800

    [optimize][项目配置]:格式化 uuid

--- blue/src/main/java/com/sumian/blue/constant/BlueConstant.java
+++ blue/src/main/java/com/sumian/blue/constant/BlueConstant.java
@@ -10,12 +10,11 @@ import java.util.UUID;
 
 public interface BlueConstant {
 
-    UUID SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+    UUID SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcaa9e");
 
-    UUID NOTIFY_UUID = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
-    UUID DESCRIPTORS_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+    UUID NOTIFY_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcaa9e");
+    UUID DESCRIPTORS_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-c50e24dcca9e");
 
-    UUID WRITE_UUID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
-
+    UUID WRITE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e2edcca9e");
 
 }
